8edb84d782a0037fd68b3a7c364d025da51bf591
John Tangarife <jtangarife@isf.com.co>
2017-07-06 15:11:13 -0500

    Sudoku solver class

---
+++ personal/sudoku_solver/main.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+from sudoku_solver import SudokuSolver
+
+def main():
+    GRID_VALUES = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    GRID_VALUES_HARD = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+    ROWS = 'ABCDEFGHI'
+    COLUMNS = '123456789'
+
+    BOXES = cross(ROWS, COLUMNS)
+    ROW_UNITS = [cross(r, COLUMNS) for r in ROWS]
+    COLUMN_UNITS = [cross(ROWS, c) for c in COLUMNS]
+    SQUARE_UNITS = [cross(rs, cs) for rs in (ROWS[:3], ROWS[3:6], ROWS[6:]) for cs in (COLUMNS[:3], COLUMNS[3:6], COLUMNS[6:])]
+    UNIT_LIST = ROW_UNITS + COLUMN_UNITS + SQUARE_UNITS
+
+    solver = SudokuSolver(BOXES, UNIT_LIST)
+    board_normal = solver.grid_values(GRID_VALUES)
+    board_hard = solver.grid_values(GRID_VALUES_HARD)
+
+    print("-- Normal: Unsolved --")
+    print("##############################")
+    solver.display(solver.grid_values(GRID_VALUES), ROWS, COLUMNS)
+
+    # Setting all posibilities on empty spaces
+    all_choises_board = solver.grid_all_posibilities(GRID_VALUES)
+
+    # Constraints propagation https://youtu.be/aSYDBcbvC5Y
+    print("-- Normal: Solved --")
+    print("##############################")
+    solver.display(solver.reduce_puzze(all_choises_board), ROWS, COLUMNS)
+
+    all_choises_board_hard = solver.grid_all_posibilities(GRID_VALUES_HARD)
+
+    # Hard Sudoku
+    print("-- Hard: Unsolved --")
+    print("##############################")
+    solver.display(solver.grid_values(GRID_VALUES_HARD), ROWS, COLUMNS)
+
+    # Reduce does not work anymore with eleminate and only choise technique
+    print("-- Hard: Unsolved but reduced --")
+    print("##############################")
+    solver.display(solver.reduce_puzze(all_choises_board_hard), ROWS, COLUMNS)
+
+def cross(a, b):
+    return [s+t for s in a for t in b]
+
+if __name__ == '__main__':
+    main()
---
+++ personal/sudoku_solver/sudoku_solver.py
@@ -0,0 +1,105 @@
+# Class https://docs.python.org/3/tutorial/classes.html
+class SudokuSolver:
+
+    def __init__(self, boxes, unitlist):
+        self.boxes = boxes
+        self.unitlist = unitlist
+
+    # My solution
+    def set_boxes_values(values):
+        board = {}
+        for index in range(len(self.boxes)):
+            board[self.boxes[index]] = values[index]
+        return board
+
+    #Better solution
+    def grid_values(self, values):
+        assert len(values) == 81, "Input grid must be a string of length 81 (9x9)"
+        return dict(zip(self.boxes, values))
+
+    # The elimination technique https://youtu.be/6rFOX2jHB2g
+    #Adding Grid values with elimination technique so we can add possible values to grid
+    def grid_all_posibilities(self, values):
+        assert len(values) == 81, "Input grid must be a string of length 81 (9x9)"
+        board = {}
+        for index in range(len(self.boxes)):
+            value = values[index]
+            if value == ".":
+                board[self.boxes[index]] = "123456789"
+            else:
+                board[self.boxes[index]] = value
+        return board
+
+    # Udacity solution
+    def another_grid_all_posibilities(values):
+        values = []
+        all_digits = '123456789'
+        for c in grid:
+            if c == '.':
+                values.append(all_digits)
+            elif c in all_digits:
+                values.append(c)
+        assert len(values) == 81
+        return dict(zip(self.boxes, values))
+
+    # Find board places with one digit element and discard from peers other options
+    def __eliminate(self, values):
+
+        units = dict((s, [u for u in self.unitlist if s in u]) for s in self.boxes)
+        peers = dict((s, set(sum(units[s],[]))-set([s])) for s in self.boxes)
+
+        solved_values = [box for box in values.keys() if len(values[box]) == 1]
+        for box in solved_values:
+            digit = values[box]
+            for peer in peers[box]:
+                values[peer] = values[peer].replace(digit, '')
+        return values
+
+    # Only choise technique: https://youtu.be/sSjYn-Kex1A
+    def __only_choise(self, values):
+        for unit in self.unitlist:
+            for digit in '123456789':
+                dplaces = [box for box in unit if digit in values[box]]
+                if len(dplaces) == 1:
+                    values[dplaces[0]] = digit
+        return values
+
+    # Constraints propagation on solving puzzle
+    def reduce_puzze(self, values):
+        stalled = False
+        while not stalled:
+            # Check how many boxes have a determined value
+            solve_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+            #Use eliminate strategy
+            self.__eliminate(values)
+
+            #Use Only choise strategy
+            self.__only_choise(values)
+
+            #Check how many boxes have a determined value to compare
+            solve_values_after = len([box for box in values.keys() if len(values[box]) == 1 ])
+
+            #If no new values were added, stop the loop.
+            stalled = solve_values_before == solve_values_after
+
+            #Sanity check: return false if there is a box with zero available values:
+            if len([box for box in values.keys() if len(values[box]) == 0]):
+                return False
+
+        return values
+
+    def display(self, values, rows, columns):
+
+        """
+        Display the values as a 2-D grid.
+        Input: The sudoku in dictionary form
+        Output: None
+        """
+        width = 1+max(len(values[s]) for s in self.boxes)
+        line = '+'.join(['-'*(width*3)]*3)
+        for r in rows:
+            print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                          for c in columns))
+            if r in 'CF': print(line)
+        return
